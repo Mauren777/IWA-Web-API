0210a4378f222df95f7f484ec26d849b5491b81b
Mauren777 <maura.tangara@gmail.com>
2020-04-23 20:27:27 +0000

    js functions to interact with my API

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# IWA-Web-API
-IWA CA2 Web API
+# IWA Web API
+Maura Carola Tangara Cuba
+
+Student ID: 2017241
+
+Project Title: Rest API for LosViajesdeMaura Travel Blog & Shop
+
+Heroku App: 
+https://web-api-ca2.herokuapp.com/ 
--- views/js/LosViajesdeMaura.js
+++ views/js/LosViajesdeMaura.js
@@ -40,13 +40,13 @@ function buildProductTable(data) {
             +  '<button class="btn btn-sm btn-primary add-to-cart" data-product-title="'+data[index].title+'" data-product-price="'+data[index].price+'">Add to cart</button>'
             +  '</td></tr>';
     });
-    $('#shopTable tbody').append(productsHTML);
+    $('#shopTable tbody').html(productsHTML);
 }
 
 function buildAdminTable(data) {
     let productsHTML = '';
     $.each(data, function(index) {
-        productsHTML += '<tr>'
+        productsHTML += '<tr id="'+data[index]._id+'">'
             +  '<td align="center">'
             +  '<button class="btn btn-sm btn-secondary edit-row" data-product-id="'+data[index]._id+'">Edit</button>'
             +  '</td>'
@@ -55,7 +55,7 @@ function buildAdminTable(data) {
             +  '<td align="right">'+data[index].price+'</td>'
             +  '</tr>';
     });
-    $('#adminTable tbody').append(productsHTML);
+    $('#adminTable tbody').html(productsHTML);
 }
 
 $(document).ready(function() {
@@ -83,5 +83,94 @@ $(document).ready(function() {
         // Add Product to list
         $('#product-list').append('<div class="col-md-12"><h6>'+ productTitle +'</h6><hr/></div>');
     });
+
+    // Select Row for Edit/Delete
+    $('body').on('click', '.edit-row', function() {
+        productId = $(this).data('product-id');
+
+        $.ajax({
+            url: '/v1/products/' + productId,
+            type: 'GET',
+            dataType: 'json',
+            cache: false, // Appends _={timestamp} to the request query string
+            success: function(data) {
+                // data is a json object.
+                $("input[name='product_id']").val(data._id);
+                $("input[name='title']").val(data.title);
+                $("#description").val(data.description);
+                $("input[name='price']").val(data.price);
+            }
+        });
+
+        $('#createBtn').hide();
+        $('#editBtns').show();
+    });
+
+    // Create new Product
+    $('body').on('click', '#create', function() {
+        $.ajax({
+            url: '/v1/products/',
+            type: 'POST',
+            data: { 
+                title: $("input[name='title']").val(),  
+                description: $("#description").text(), 
+                price: $("input[name='price']").val()
+            },
+            dataType: 'json',
+            success: function(data) {
+                // Reinit updated admin table
+                adminTable();
+            }
+        });
+    });
+
+    // Edit Product
+    $('body').on('click', '#edit', function() {
+        // If we have our product id
+        if($('#product_id').val()) {
+            $.ajax({
+                url: '/v1/products/' + $('#product_id').val(),
+                type: 'POST',
+                data: { 
+                    title: $("input[name='title']").val(),  
+                    description: $("#description").val(), 
+                    price: $("input[name='price']").val()
+                },
+                dataType: 'json',
+                success: function(data) {
+                    // Reload updated admin table
+                    adminTable();
+                }
+            });
+        }
+    });
+
+    // Delete Product
+    $('body').on('click', '#delete', function() {
+        // If we have our product id
+        if($('#product_id').val()) {
+            $.ajax({
+                url: '/v1/products/' + $('#product_id').val(),
+                type: 'DELETE',
+                dataType: 'json',
+                success: function(data) {
+                    // Reload updated admin table
+                    adminTable();
+                }
+            });
+        }
+    });
+
+    // Cancel Edit 
+    $('body').on('click', '#cancel', function() {
+        $('#adminForm')[0].reset()
+        $('#createBtn').show();
+        $('#editBtns').hide();
+    });
+
+    // prevent default form submit
+    $(document).on('submit','#adminForm',function(e){
+        e.preventDefault();
+    })
     
 });
