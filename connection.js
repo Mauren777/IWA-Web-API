e10306f67bf1ce354f5ad1943329a9023bc70a64
Mauren777 <maura.tangara@gmail.com>
2020-04-24 15:26:48 +0000

    Fix connect.js

--- connection.js
+++ connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-
 // Connect to MongoDB Atlas. User and Password are Heroku config Values
-mongoose.connect('mongodb+srv://process.env.MONGODB_USERNAME:process.env.MONGODB_PASSWORD@cluster0-rtat4.mongodb.net/shop?retryWrites=true&w=majority', { useNewUrlParser: true,  useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@cluster0-rtat4.mongodb.net/shop?retryWrites=true&w=majority', { useNewUrlParser: true,  useUnifiedTopology: true });
 
 mongoose.connection.on('error', (err) => { 
     console.log('Mongodb Error: ', err); 
